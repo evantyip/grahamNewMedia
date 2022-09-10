12138ac30858a20baafa54594eae00a1d031effd
Evan Yip <etyip@ucsb.edu>
2022-09-10 10:49:28 -0700

    Add Portfolio page

---
+++ pages/portfolio/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+type Props = {}
+
+const Index = (props: Props) => {
+  return (
+    <div className="container m-auto flex flex-col">
+      <div className="flex flex-row justify-center py-8">
+        <p className="text-7xl ">Portfolio</p>
+      </div>
+      <div className="my-10 flex flex-row justify-evenly">
+        <Link href="/portfolio/films">
+          <div className="Image relative my-auto ">
+            <Image
+              className="absolute"
+              src="/portfolio/portfolioFilms.jpg"
+              alt="Films picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Films
+            </p>
+          </div>
+        </Link>
+
+        <Link href="/stills">
+          <div className="Image relative my-auto">
+            <Image
+              className="absolute"
+              src="/portfolio/portfolioStills.jpg"
+              alt="Stills picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Stills
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="my-10 flex flex-row justify-center">
+        {/* TODO UPDATE LINK */}
+        <Link href="/portfolio">
+          <div className="Image relative mx-auto">
+            <Image
+              className="absolute"
+              src="/portfolio/portfolioGraphics.jpg"
+              alt="Graphics picture "
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Graphics
+            </p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Index
