e3977eae257fe95a087fcd5f92b3d4954c89aa19
Evan Yip <etyip@ucsb.edu>
2022-09-10 10:49:37 -0700

    Add Stills Page

---
+++ pages/stills/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+type Props = {}
+
+const Index = (props: Props) => {
+  return (
+    <div className="container m-auto flex flex-col">
+      <div className="flex flex-row justify-center py-8">
+        <p className="text-7xl">Stills</p>
+      </div>
+      <div className="my-10 flex flex-row justify-evenly">
+        {/* TODO UPDATE LINKS LATER */}
+        <Link href="/stills/gallery">
+          <div className="Image relative my-auto ">
+            <Image
+              className="absolute"
+              src="/stills/stillsLandscape.jpg"
+              alt="Films picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Landscape
+            </p>
+          </div>
+        </Link>
+
+        <Link href="/stills/gallery">
+          <div className="Image relative my-auto">
+            <Image
+              className="absolute"
+              src="/stills/stillsMusic.jpg"
+              alt="Stills picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Music
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="my-10 flex flex-row justify-evenly">
+        <Link href="stills/gallery">
+          <div className="Image relative my-auto ">
+            <Image
+              className="absolute"
+              src="/stills/stillsAdventure.jpg"
+              alt="Films picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl  hover:cursor-pointer">
+              Adventure
+            </p>
+          </div>
+        </Link>
+
+        <Link href="/stills/gallery">
+          <div className="Image relative my-auto">
+            <Image
+              className="absolute"
+              src="/stills/stillsCommercial.jpg"
+              alt="Stills picture"
+              width="512"
+              height="384"
+            />
+            <p className="absolute inset-0 z-10 flex items-center justify-center font-serif text-6xl text-white hover:cursor-pointer">
+              Commercial
+            </p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Index
