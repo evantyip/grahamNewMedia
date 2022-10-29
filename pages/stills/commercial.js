25724b89b177c24b4a50c0931b679feb3f7aabee
Evan Yip <etyip@ucsb.edu>
2022-10-28 20:57:22 -0700

    Add new stills pages

---
+++ pages/stills/commercial.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { contentfulClient } from '../../utility/contentful'
+import Gallery from '../../components/Gallery'
+
+const commercial = ({ gallery }) => {
+  return <Gallery gallery={gallery} />
+}
+
+export const getStaticProps = async () => {
+  const content = await contentfulClient.getEntries({
+    content_type: 'galleryPicture',
+    'fields.type': 'Commercial',
+  })
+  const gallery = content.items.map((el) => {
+    const picFields = el.fields.picture.fields
+    return {
+      title: el.fields.title,
+      type: el.fields.type,
+      url: picFields.file.url.replace('//', 'https://'),
+    }
+  })
+  return {
+    props: {
+      gallery,
+    },
+  }
+}
+export default commercial
